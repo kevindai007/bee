130b302d310570a5a2a645ee406cb7fee1bdadef
李斌松 <binsong.li@tongdun.cn>
2017-04-25 14:49:19 +0800

    update

--- src/main/java/cn/tongdun/bee/model/LoginUserDetails.java
+++ src/main/java/cn/tongdun/bee/model/LoginUserDetails.java
@@ -19,6 +19,8 @@ public class LoginUserDetails extends User {
 
     private String appkey;
 
+    private String appsecret;
+
     public LoginUserDetails(String username, String password,
                             Collection<? extends GrantedAuthority> authorities) {
         super(username, password, true, true, true, true, authorities);
@@ -51,5 +53,13 @@ public class LoginUserDetails extends User {
 
     public void setAppkey(String appkey) {
         this.appkey = appkey;
+    }
+
+    public String getAppsecret() {
+        return appsecret;
+    }
+
+    public void setAppsecret(String appsecret) {
+        this.appsecret = appsecret;
     }
 }
